8a09f3d6f079e731a0e58fbe41ab12c926547cf4
Boonyarit Iamsa-ard <boonyarit.iamsaard@gmail.com>
2021-07-07 22:14:10 +0700

    update flight model

--- app/models/flight-model.js
+++ app/models/flight-model.js
@@ -1,5 +1,77 @@
 const mongoose = require('mongoose');
 
+const extraEquipmentSchema = new mongoose.Schema({
+  company: {
+    type: String,
+    required: true,
+  },
+  equipment: {
+    type: String,
+    required: true,
+  },
+});
+
+const taskSchema = new mongoose.Schema({
+  taskNo: {
+    type: String,
+  },
+  taskDetails: {
+    type: String,
+    required: true,
+  },
+  deferral: {
+    type: String,
+  },
+  deferralNumber: {
+    type: String,
+  },
+  deferralAction: {
+    type: String,
+  },
+  engineerHours: {
+    type: Number,
+    default: 0,
+  },
+  mechanicHours: {
+    type: Number,
+    default: 0,
+  },
+});
+
+const chargeableServiceSchema = new mongoose.Schema({
+  service: {
+    type: String,
+    required: true,
+  },
+  usage: {
+    type: Number,
+    default: 0,
+  },
+  engineerHours: {
+    type: Number,
+    default: 0,
+  },
+  mechanicHours: {
+    type: Number,
+    default: 0,
+  },
+});
+
+const assignedDelaySchema = new mongoose.Schema({
+  category: {
+    type: String,
+    required: true,
+  },
+  code: {
+    type: String,
+    required: true,
+  },
+  duration: {
+    type: String,
+    required: true,
+  },
+});
+
 const flightSchema = new mongoose.Schema(
   {
     date: {
@@ -9,9 +81,6 @@ const flightSchema = new mongoose.Schema(
     airline: {
       type: String,
       required: true,
-    },
-    otherAirline: {
-      type: String,
     },
     fltno: {
       type: String,
@@ -29,24 +98,6 @@ const flightSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
-    aircraftType: {
-      type: String,
-      required: true,
-    },
-    check: {
-      type: String,
-      required: true,
-    },
-    otherCheck: {
-      type: String,
-    },
-    eic: {
-      type: String,
-      required: true,
-    },
-    handOver: {
-      type: String,
-    },
     ata: {
       type: String,
     },
@@ -54,6 +105,47 @@ const flightSchema = new mongoose.Schema(
       type: String,
     },
     bay: {
+      type: String,
+    },
+    check1: {
+      type: String,
+      required: true,
+    },
+    check2: {
+      type: String,
+    },
+    afac: {
+      type: Number,
+      default: 0,
+    },
+    water: {
+      type: Boolean,
+      default: false,
+    },
+    lavatory: {
+      type: Boolean,
+      default: false,
+    },
+    extraGroundEquipments: [extraEquipmentSchema],
+    tasks: [taskSchema],
+    chargeableServices: [chargeableServiceSchema],
+    assignedDelays: [assignedDelaySchema],
+    engineer: {
+      type: String,
+      required: true,
+    },
+    mechanic1: {
+      type: String,
+      required: true,
+    },
+    mechanic2: {
+      type: String,
+    },
+    recordBy: {
+      type: String,
+      required: true,
+    },
+    remark: {
       type: String,
     },
   },
