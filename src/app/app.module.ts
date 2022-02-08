7b305b6ffee43edcc9b7741ed223e332f188d5ea
Boonyarit Iamsa-ard <boonyarit.iamsaard@gmail.com>
2022-02-08 16:36:59 +0700

    build: add env config and validation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 
-@Module({})
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        PORT: Joi.number().default(3000),
+      }),
+    }),
+  ],
+})
 export class AppModule {}
